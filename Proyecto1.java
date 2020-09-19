417cc56a328e32cd0642d91287c4b43fc7ac31be
gonzariquelme6 <gonzariquelme66@gmail.com>
2020-09-19 15:24:02 -0300

    Modifico el archivo original Proyecto1.java

--- Proyecto1.java
+++ Proyecto1.java
@@ -1,6 +1,6 @@
 public class Proyecto1 {
 	public static void main(String[] args) {
-		Auto a = new Auto("Fiesta","Ford");
+		Auto a = new Auto("Mustang GT","Ford");
 		System.out.println("Auto: "+a.getMarca()+a.getModelo());
 	}
 }
