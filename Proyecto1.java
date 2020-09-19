cfb78e268bd0e3383bafe680d0a7ac85a04b54e9
gonzariquelme6 <gonzariquelme66@gmail.com>
2020-09-19 15:16:01 -0300

    Modifico el archivo Proyecto1 en la copia

--- Proyecto1.java
+++ Proyecto1.java
@@ -1,6 +1,6 @@
 public class Proyecto1 {
 	public static void main(String[] args) {
-		Auto a = new Auto("Fiesta","Ford");
+		Auto a = new Auto("Focus","Ford");
 		System.out.println("Auto: "+a.getMarca()+a.getModelo());
 	}
 }
